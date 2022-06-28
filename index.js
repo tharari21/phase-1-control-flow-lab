0972211240f95b3fe12e0dbec38c753ac6e527f5
Tomer Harari <tharari21@gmail.com>
2022-06-27 22:33:05 -0400

    commit tree b7e385a2f2505aa37f8d9884e1df7e4ba493b102

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance) {
   // Write your code here!
+  let result;
+  if (rideDistance <= 400) {
+    result = "This one is on me!";
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (rideDistance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+  if (city === "NYC") {
+    result = "Ok, sounds good.";
+  } else {
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let result;
+  if (tip === "generous") {
+    result = "Thank you so much.";
+  } else if (tip === "not as generous") {
+    result = "Thank you.";
+  } else {
+    result = "Bye.";
+  }
+  return result;
 }
